8125adefa04248b62dedeb8db386d84b07a22d4e
Head Gardener <trashbin2019np@gmail.com>
2023-09-30 19:38:16 +0300

    migrated to yesod

--- app/Main.hs
+++ app/Main.hs
@@ -1,158 +1,156 @@
-module Main (main) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-import Control.Exception (bracket)
-import Control.Monad (msum)
-import Control.Monad.Reader (asks)
-import Control.Monad.State (get, put)
-import Data.Acid
-  ( AcidState,
-    Query,
-    Update,
-    makeAcidic,
-    openLocalState,
-  )
-import Data.Acid.Advanced (query', update')
-import Data.Acid.Local (createCheckpointAndClose)
-import Data.Data (Data)
-import Data.Dynamic (Typeable)
-import Data.SafeCopy (base, deriveSafeCopy)
+import Control.Monad.Logger (runStderrLoggingT)
+import Control.Monad.Trans.Resource (runResourceT)
 import Data.Text (Text)
-import Happstack.Server (BodyPolicy, Browsing (DisableBrowsing), Method (GET, POST), decodeBody, defaultBodyPolicy, dir, lookText', method, nullConf, ok, seeOther, serveDirectory, simpleHTTP)
-import Happstack.Server.SimpleHTTP
-  ( Response,
-    ServerPartT,
-    ToMessage (toResponse),
-  )
-import Text.Blaze.Html5 (Html, toHtml, (!))
-import Text.Blaze.Html5 qualified as H
-import Text.Blaze.Html5.Attributes qualified as A
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+import Yesod
 
--- STATE
+newtype Live = Live ConnectionPool
 
-data Post = Post
-  { author :: Text,
-    content :: Text
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+Post
+  author Text
+  content Text
+  deriving Show
+|]
+
+mkYesod
+  "Live"
+  [parseRoutes|
+/ HomeR GET POST
+/post/#PostId PostR GET
+|]
+
+instance Yesod Live where
+  defaultLayout widget = do
+    let style :: Widget = toWidget [lucius| h2 { color: green; } |]
+    pc <- widgetToPageContent $ widget <> style
+    withUrlRenderer
+      [hamlet|
+                $doctype 5
+                <html>
+                    <head>
+                        <title>#{pageTitle pc}
+                        ^{pageHead pc}
+                        <meta name=keywords content="some sample keywords">
+                    <body>
+                        ^{pageBody pc}
+            |]
+
+instance YesodPersist Live where
+  type YesodPersistBackend Live = SqlBackend
+
+  runDB action = do
+    Live pool <- getYesod
+    runSqlPool action pool
+
+instance RenderMessage Live FormMessage where
+  renderMessage _ _ = defaultFormMessage
+
+getPostR :: PostId -> Handler Html
+getPostR postId = do
+  post <- runDB $ selectList [PostId ==. postId] [LimitTo 1]
+  case post of
+    [] -> notFound
+    p : _ ->
+      defaultLayout $ do
+        setTitle . toHtml . ("Post by " <>) . postAuthor . entityVal $ p
+        drawPost (entityVal p) Nothing
+        homeHyper
+        postStyle
+
+getHomeR :: Handler Html
+getHomeR = do
+  (widget, enctype) <- generateFormPost postForm
+  posts <- runDB $ selectList [] []
+  defaultLayout $ do
+    setTitle "Live"
+    submitForm widget enctype
+    drawPosts posts
+    postStyle
+
+postHomeR :: Handler Html
+postHomeR = do
+  ((result, _), _) <- runFormPost postForm
+  case result of
+    FormSuccess post -> do
+      key <- runDB $ insert post
+      liftIO $ print key
+      return ()
+    _ -> return ()
+  redirect HomeR
+
+homeHyper :: Widget
+homeHyper = toWidget [hamlet|<a href=@{HomeR}>go back|]
+
+postStyle :: Widget
+postStyle =
+  toWidget
+    [lucius|
+  .post { 
+    background: green;
+    margin: 5px 0px 5px 0px;
   }
-  deriving (Eq, Ord, Read, Show, Data, Typeable)
+  .post-author { color: #aaa }
+  .post-id { color: #aaa }
+|]
 
-$(deriveSafeCopy 0 'base ''Post)
+postForm :: Html -> MForm Handler (FormResult Post, Widget)
+postForm =
+  renderDivs $
+    Post
+      <$> areq textField "Author" Nothing
+      <*> areq textField "Text" Nothing
 
-newtype PostState = PostState {ps :: [Post]}
-  deriving (Eq, Ord, Read, Show, Data, Typeable)
+drawPost :: Post -> Maybe PostId -> Widget
+drawPost (Post author content) postId = do
+  toWidget
+    [hamlet|
+    $maybe i <- postId
+      <a href=@{PostR i}>
+        <div .post >
+          <p .post-fill> #{content}
+          <p .post-author> by <b>#{author}</b>
+    $nothing
+      <div .post >
+        <p .post-fill> #{content}
+        <p .post-author> by <b>#{author}</b>
+    |]
 
-$(deriveSafeCopy 0 'base ''PostState)
-
-initialPostState :: PostState
-initialPostState = PostState posts
+drawPosts :: [Entity Post] -> Widget
+drawPosts = foldr (flip (>>) . wrap drawPost) header
   where
-    posts :: [Post]
-    posts = [Post "admin" "Hello, World!", Post "admin" "More data!"]
+    header = toWidget [hamlet|<h2> Posts:|]
+    wrap f e = f (entityVal e) $ Just (entityKey e)
 
-newPost :: Post -> Update PostState [Post]
-newPost p = do
-  s <- get
-  let new = p : ps s
-  put $ s {ps = new}
-  return new
+submitForm :: Widget -> Enctype -> Widget
+submitForm form enc = do
+  toWidget
+    [whamlet|
+  <h2> Submit: 
+  <form method=post action=@{HomeR} enctype=#{enc}>
+            ^{form}
+            <button>Submit
+  |]
 
-purge :: Update PostState [Post]
-purge = do
-  s <- get
-  put $ s {ps = []}
-  return []
-
-peekPosts :: Query PostState [Post]
-peekPosts = asks ps
-
-$(makeAcidic ''PostState ['peekPosts, 'newPost, 'purge])
-
--- HANDLERS
+openConnectionCount :: Int
+openConnectionCount = 10
 
 main :: IO ()
-main = withState (simpleHTTP nullConf . homePage)
-  where
-    withState =
-      bracket
-        (openLocalState initialPostState)
-        createCheckpointAndClose
-
-myPolicy :: BodyPolicy
-myPolicy = defaultBodyPolicy "/tmp/" 0 1000 1000
-
-homePage :: AcidState PostState -> ServerPartT IO Response
-homePage acid = do
-  msum
-    [ dir "static" $ serveDirectory DisableBrowsing [] "res",
-      -- control
-      dir "purge" $ do
-        method GET
-        _ <- update' acid Purge
-        seeOther ("/" :: Text) plug,
-      do
-        method POST
-        decodeBody myPolicy
-        getPost >>= seeOther ("/" :: Text),
-      -- pages
-      method GET >> home
-    ]
-  where
-    home = do
-      ps <- query' acid PeekPosts
-      ok $ template "live" $ do
-        submitForm
-        drawPosts ps
-
-    getPost = do
-      a <- lookText' "author"
-      c <- lookText' "content"
-      let p = Post a c
-      if valid p
-        then update' acid (NewPost p) >> return plug
-        else return plug
-
-    valid (Post "" _) = False
-    valid (Post _ "") = False
-    valid _ = True
-
-    plug = toResponse ()
-
--- VIEW
-
-drawPost :: Post -> Html
-drawPost p = H.div ! A.class_ "post" $ do
-  H.p $ toHtml $ content p
-  H.p $ toHtml ("by: " :: Text) <> H.b (toHtml $ author p)
-
-drawPosts :: (Foldable a) => a Post -> Html
-drawPosts = foldr (flip (>>) . drawPost) header
-  where
-    header = H.html $ H.h1 "Posts:"
-
-submitForm :: Html
-submitForm = do
-  H.h1 "Submit:"
-  H.form
-    ! A.enctype "multipart/formdata"
-    ! A.action "/"
-    ! A.method "POST"
-    $ do
-      H.label ! A.for "name" $ "Name:"
-      H.input ! A.type_ "text" ! A.name "author" ! A.id "author"
-      H.br
-      H.label ! A.for "text" $ "Text:"
-      H.input ! A.type_ "text" ! A.name "content" ! A.id "content"
-      H.br
-      H.input ! A.type_ "submit" ! A.value "Submit"
-
-template :: Text -> Html -> Response
-template title body = toResponse $
-  H.docTypeHtml $ do
-    H.head $ do
-      H.link ! A.rel "stylesheet" ! A.href "static/styles.css"
-      H.link ! A.rel "stylesheet" ! A.href "https://fonts.googleapis.com/css?family=Ubuntu"
-      H.title $ toHtml title
-    H.body $ do
-      body
-      footer
-  where
-    footer = H.div ! A.class_ "footer" $ H.p "don't screw this up"
+main = do
+  runStderrLoggingT $ withSqlitePool "state/db.sqlite" openConnectionCount $ \pool -> liftIO $ do
+    runResourceT $ flip runSqlPool pool $ do
+      runMigration migrateAll
+    warp 3000 $ Live pool
